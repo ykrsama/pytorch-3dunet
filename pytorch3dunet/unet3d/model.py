52ceae42a9bd12564cb25bab86cebc50a40d00e4
Xuliang Zhu <xuliangz@sjtu.edu.cn>
2026-04-16 10:35:00 +0000

    remove fpgamodular

--- pytorch3dunet/unet3d/model.py
+++ pytorch3dunet/unet3d/model.py
@@ -4,7 +4,6 @@ from torch import nn
 from pytorch3dunet.unet3d.buildingblocks import DoubleConv, ResNetBlock, ResNetBlockSE, \
     create_decoders, create_encoders
 from pytorch3dunet.unet3d.utils import get_class, number_of_features_per_level
-from pytorch3dunet.unet3d.fpga_unet_modular import UNet3DFPGAModular
 
 
 class AbstractUNet(nn.Module):
